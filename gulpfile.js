6f9d225bd936540e586618b1847bb22527181427
phan thanh phuong <phuongit0301@mytrax.co.jp>
2015-10-02 16:52:01 +0700

    Install grunt

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
+
+gulp.task("copyfiles", function() {
+
+	gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+	    .pipe(gulp.dest("resources/assets/js"));
+
+	gulp.src("vendor/bower_dl/bootstrap/less/**")
+		.pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+	gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+		.pipe(gulp.dest("resources/assets/js/"));
+
+	gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+		.pipe(gulp.dest("public/assets/fonts"));
+
+});
+
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +30,14 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+	mix.scripts([
+		'js/jquery.js',
+		'js/bootstrap.js'
+		],
+		'public/assets/js/admin.js',
+		'resources/assets'
+	);
+
+    mix.less('admin.less', 'public/assets/css/admin.css');
 });
